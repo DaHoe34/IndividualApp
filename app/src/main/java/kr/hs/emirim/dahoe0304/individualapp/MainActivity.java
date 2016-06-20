64fc16ccb817541de4cafdc7661166f65db42887
Dahoe Shin <kity990304@gmail.com>
2016-06-20 20:46:30 +0900

    #2 splash

--- app/src/main/java/kr/hs/emirim/dahoe0304/individualapp/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dahoe0304/individualapp/MainActivity.java
@@ -2,12 +2,25 @@ package kr.hs.emirim.dahoe0304.individualapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    EditText editNum1;
+    TextView textResult;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textResult=(TextView)findViewById(R.id.text_result);
+    }
+    public void onClick(View v){
+        String num1Str=editNum1.getText().toString();
+        int num1=Integer.parseInt(num1Str);
+        double result=0;
+
+        textResult.setText("계산 결과 : "+result);
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/dahoe0304/individualapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package kr.hs.emirim.dahoe0304.individualapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by appcreator18 on 2016. 6. 20..
+ */
+public class SplashActivity extends AppCompatActivity{
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0,R.anim.zoom_exit);
+                finish();
+            }
+        },3000);
+    }
+}
